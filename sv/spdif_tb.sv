8c2bf2372ee2d51c35b43a101479b8a1d2c5d4c5
zsassi <zied.sassi@gmail.com>
2014-01-11 16:01:22 -0800

    Create spdif_tb.sv

---
+++ sv/spdif_tb.sv
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////SPDIF GENERATOR BY zied.sassi@gmail.com /////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+//------------------------------------------------------------------------------
+//
+// CLASS: spdif_tb
+//
+//------------------------------------------------------------------------------
+`include "spdif_seq_lib.sv"
+class spdif_tb extends uvm_env;
+
+  `uvm_component_utils(spdif_tb)
+  spdif_env m_env0;
+  // new
+  function new (string name, uvm_component parent=null);
+    super.new(name, parent);
+  endfunction : new
+
+  // build
+  virtual function void build();
+    super.build();
+    m_env0 = spdif_env::type_id::create("m_env0", this);
+  endfunction : build
+
+  function void connect();
+    m_env0.assign_vi(spdif_tb_top.m_if);
+  endfunction : connect
+  
+  function void end_of_elaboration();
+  endfunction : end_of_elaboration
+
+endclass : spdif_tb
+
+`endif // SPDIF_TB_SV
+
